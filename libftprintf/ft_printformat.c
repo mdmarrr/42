070dd0418c0df50bad711b73d6b2ee3486b0f747
Maria Del Mar Gomez Del Valle <magomez-@c2r4s2.42malaga.com>
2025-12-04 14:23:56 +0100

    update printf

--- libftprintf/ft_printformat.c
+++ libftprintf/ft_printformat.c
@@ -6,32 +6,38 @@
 /*   By: magomez- <magomez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 13:14:16 by magomez-          #+#    #+#             */
-/*   Updated: 2025/11/18 10:03:58 by magomez-         ###   ########.fr       */
+/*   Updated: 2025/12/04 12:57:00 by magomez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	print_format(char specifier, va_list ap)
+int	print_format(char specifier, va_list args)
 {
 	int	count;
 
 	count = 0;
 	if (specifier == 'c')
-		count += ft_print_char(va_arg(ap, int));
+		count += ft_print_char(va_arg(args, int));
 	else if (specifier == 's')
-		count += ft_print_str(va_arg(ap, char *));
+		count += ft_print_str(va_arg(args, char *));
 	else if (specifier == 'd' || specifier == 'i')
-		count += ft_print_nbr(((long)va_arg(ap, int)), 10);
+		count += ft_print_nbr(((long)va_arg(args, int)), 10);
 	else if (specifier == 'u')
-		count += ft_print_nbr(va_arg(ap, unsigned int), 10);
+		count += ft_print_nbr(va_arg(args, unsigned int), 10);
 	else if (specifier == 'x')
-		count += ft_print_nbr(va_arg(ap, unsigned int), 16);
+		count += ft_print_nbr(va_arg(args, unsigned int), 16);
 	else if (specifier == 'X')
-		count += ft_print_nbr_caps(va_arg(ap, unsigned int), 16);
+		count += ft_print_nbr_caps(va_arg(args, unsigned int), 16);
 	else if (specifier == 'p')
-		count += ft_print_pointer(va_arg(ap, void *));
+		count += ft_print_pointer(va_arg(args, void *));
 	else
 		count += write(1, &specifier, 1);
 	return (count);
+}
+
+int	ft_print_char(int c)
+{
+	write(1, &c, 1);
+	return (1);
 }
