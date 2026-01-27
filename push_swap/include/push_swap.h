5a0abe469a26c37461bfccd2624f71311c327c73
Maria Del Mar Gomez Del Valle <magomez-@c3r8s4.42malaga.com>
2026-01-27 13:29:55 +0100

    upload operations

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: magomez- <magomez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 13:14:16 by magomez-          #+#    #+#             */
-/*   Updated: 2026/01/27 11:56:48 by magomez-         ###   ########.fr       */
+/*   Updated: 2026/01/27 13:28:37 by magomez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,18 @@ int		ft_check_dup(t_stack *a);
 int		ft_check_sorted(t_stack *stack_a);
 void	ft_free(t_stack **lst);
 void	ft_parse(int argc, char **argv);
+
+void	fr_sa(t_stack **a, int j);
+void	ft_sb(t_stack **b, int j);
+void	fr_ss(t_stack **a, t_stack **b, int j);
+void	fr_pa(t_stack **a, t_stack **b, int j);
+void	ft_pb(t_stack **stack_a, t_stack **stack_b, int j);
+void	ft_ra(t_stack **a, int j);
+void	ft_rb(t_stack **b, int j);
+void	ft_rr(t_stack **a, t_stack **b, int j);
+void	fr_rra(t_stack **a, int j);
+void	ft_rrb(t_stack **b, int j);
+void	ft_rrr2(t_stack **a, t_stack **b, int j);
+void	ft_rrr(t_stack **a, t_stack **b, int j);
 
 #endif
---
+++ push_swap/src/ft_operations3.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_operations3.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: magomez- <magomez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 13:14:16 by magomez-          #+#    #+#             */
+/*   Updated: 2026/01/27 13:26:21 by magomez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+void	ft_rb(t_stack **b, int j)
+{
+	t_stack	*tmp;
+
+	if (!*b || !(*b)->next)
+		return ;
+	tmp = *b;
+	*b = ft_lstlast(*b);
+	(*b)->next = tmp;
+	*b = tmp->next;
+	tmp->next = NULL;
+	if (j == 0)
+		write(1, "rb\n", 3);
+}
+
+void	ft_sb(t_stack **b, int j)
+{
+	t_stack	*tmp;
+
+	if (!*b || !((*b)->next))
+		return ;
+	tmp = *b;
+	*b = (*b)->next;
+	tmp->next = (*b)->next;
+	(*b)->next = tmp;
+	if (j == 0)
+		write(1, "sb\n", 3);
+}
