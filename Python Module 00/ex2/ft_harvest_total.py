b81f13a64a663812e83557f0ad1f613d587e9310
mdm <mariaadelmaar97@gmail.com>
2026-02-26 20:25:08 +0100

    Python module 0

---
+++ Python Module 00/ex2/ft_harvest_total.py
@@ -0,0 +1,6 @@
+def ft_harvest_total():
+    weigth1 = int(input("Day 1 harvest: "))
+    weigth2 = int(input("Day 2 harvest: "))
+    weigth3 = int(input("Day 3 harvest: "))
+    total = weigth1 + weigth2 + weigth3
+    print("Total harvest:", total)
