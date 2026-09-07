29589a2a65fbf51a84b0c4f79cd5e84b63a622c3
María del mar <102030848+mdmarrr@users.noreply.github.com>
2026-09-07 14:23:29 +0200

    exam rank 03 level 4

--- Milestone 3/rank03/level04/py_anagram.py
+++ Milestone 3/rank03/level04/py_anagram.py
@@ -2,11 +2,4 @@ def anagram(s1: str, s2: str) -> bool:
 	s1 = s1.replace(" ", "").lower()
 	s2 = s2.replace(" ", "").lower()
 
-	if len(s1) != len(s2):
-		return False
-	
-	for char in s1:
-		if s1.count(char) != s2.cpunt(char):
-			return False
-		
-	return True
+	return sorted(s1) == sorted(s2)
--- Milestone 3/rank03/level04/py_shadow_merge.py
+++ Milestone 3/rank03/level04/py_shadow_merge.py
@@ -11,12 +11,7 @@ def shadow_merge(list1: list[int], list2: list[int]) -> list[int]:
 			result.append(list2[j])
 			j += 1
 	
-	while i < len(list1):
-		result.append(list1[i])
-		i += 1
-	
-	while j < len(list2):
-		result.append(list2[j])
-		j += 1
+	result.extend(list1[i:])
+    	result.extend(list2[j:])
 	
 	return result
