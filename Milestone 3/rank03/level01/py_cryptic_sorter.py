4e8c3d9155a39922ef6883b56f57efe5441501b4
María del mar <102030848+mdmarrr@users.noreply.github.com>
2026-09-10 17:58:03 +0200

    Refactor cryptic_sorter to use sorted function

--- Milestone 3/rank03/level01/py_cryptic_sorter.py
+++ Milestone 3/rank03/level01/py_cryptic_sorter.py
@@ -1,26 +1,5 @@
 def cryptic_sorter(strings: list[str]) -> list[str]:
-	result = []
+    def key(s):
+        return (len(s), s.lower(), sum(c.lower() in "aeiou" for c in s))
 
-	def vowels_count(s: str) -> int:
-		count = 0
-		for char in s:
-			if char.lower() in "aeiou":
-				count += 1
-		return count
-	
-	def key(s: str):
-		return (
-			len(s),
-			s.lower(),
-			vowels_count(s)
-		)
-	
-	for string in strings:
-		i = len(result)
-
-		while i > 0 and key(result[i -1]) > key(string):
-			i -= 1
-		
-		result.insert(i, string)
-	
-	return result
+    return sorted(strings, key=key)
